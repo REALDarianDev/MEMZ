25aa2381605457bf5265277df71380d13c5af317
Darian Mohaseb <73662785+REALDarianDev@users.noreply.github.com>
2020-11-17 14:41:24 -0500

    Create RUN THIS AS ADMIN.bat

---
+++ RUN THIS AS ADMIN.bat
@@ -0,0 +1,19 @@
+@Echo OFF
+AT > NUL
+IF %ERRORLEVEL% EQU 0 (
+    GOTO :START:
+) ELSE (
+    ECHO Admin Rights NEEDED to run this file!
+    GOTO :EXIT:
+    
+)
+:START:
+Echo YOU SURE BOI? THIS MIGHT BRAKE YA PC..
+SET /P Y/N= Y/N 
+IF "%Y/N%"=="N" GOTO :EXIT:
+START MEME.bat
+
+
+:EXIT:
+pause
+exit
